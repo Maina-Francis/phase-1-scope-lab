39674dc2fc609a71ace3b707f102734fc5ac7b8d
Francis Maina <sebastianfrancix@gmail.com>
2022-08-23 01:00:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// console.log(upperCaseCustomerName());
+// console.log(customerName);
+
+var bestCustomer;
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+// setBestCustomer();
+// console.log(bestCustomer);
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+// overwriteBestCustomer();
+// console.log(bestCustomer);
+
+const leastFavoriteCustomer = "Shaq";
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "Bruno";
+}
